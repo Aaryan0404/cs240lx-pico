2e2c72cbc66664a7a693be89d90a94f96f54cd80
AaryanSinghal <aaryan0404@gmail.com>
2024-04-09 20:49:40 -0700

    notmain blink

---
+++ bootloader/notmain.c
@@ -0,0 +1,15 @@
+#include "gpio.h"
+#include <stdint.h>
+
+void notmain() {
+    gpio_reset();
+    
+    gpio_set_output(25); 
+
+    while (1) {
+        gpio_set_on(25);
+        DELAY(0x100000);
+        gpio_set_off(25);
+        DELAY(0x100000);
+    }
+}
