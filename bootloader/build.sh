dc6c4bf045b604c99d8425ab4c9ff3cb8830a4f0
Joseph Shetaye <joseph@shetaye.me>
2024-04-09 21:27:41 -0700

    UF2 conversion & blinking

--- bootloader/build.sh
+++ bootloader/build.sh
@@ -1,6 +1,10 @@
 #/bin/sh
 
+arm-none-eabi-gcc -mcpu=cortex-m0plus -mtune=cortex-m0plus -g -c gpio.c
+arm-none-eabi-gcc -mcpu=cortex-m0plus -mtune=cortex-m0plus -g -c notmain.c
 arm-none-eabi-gcc -mcpu=cortex-m0plus -mtune=cortex-m0plus -g -c start.S
-arm-none-eabi-ld -o pi.elf -T memmap.ld start.o
+arm-none-eabi-ld -o pi.elf -T memmap.ld start.o notmain.o gpio.o
 arm-none-eabi-objdump -d pi.elf > pi.list
 arm-none-eabi-objcopy -O binary pi.elf pi.bin
+cat pi.bin | python3 patch-crc.py > pi-patched.bin
+tools/uf2conv/uf2conv.py pi-patched.bin --family rp2040 -b 0x10000000 --convert --output pi.uf2
--- bootloader/notmain.c
+++ bootloader/notmain.c
@@ -1,15 +1,20 @@
 #include "gpio.h"
 #include <stdint.h>
 
+void cstart() {
+  void notmain();
+  notmain();
+}
+
 void notmain() {
     gpio_reset();
     
-    gpio_set_output(25); 
+    gpio_set_output(15); 
 
     while (1) {
-        gpio_set_on(25);
+        gpio_set_on(15);
         DELAY(0x100000);
-        gpio_set_off(25);
+        gpio_set_off(15);
         DELAY(0x100000);
     }
 }
